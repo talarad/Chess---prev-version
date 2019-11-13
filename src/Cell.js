b6e344f9f67e3bdd2ca9cde9a0b6d3c50a950120
Tal Arad <talarad5@gmail.com>
2019-11-13 18:20:37 +0200

    fixed board colors

--- src/Cell.js
+++ src/Cell.js
@@ -2,50 +2,95 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 export default function Cell(props) {
-    if (props.cell.side === 'black') {
-        if (props.cell.clicked) {
-            return (
-                <div className="black-cell-clicked"  >
-                    <img src={require(`./pieces/${props.cell.side}/${props.cell.name}.png`)} onClick={() => props.onCellClick(props.row, props.column)} alt="red" />
-                </div>
-            )
-        } else {
-            if (props.side) {
+    if ((props.row + props.column) % 2 !== 0) {
+        if (props.cell.side === 'black') {
+            if (props.cell.clicked) {
                 return (
-                    <div className="black-cell"  >
+                    <div className="black-cell-clicked"  >
                         <img src={require(`./pieces/${props.cell.side}/${props.cell.name}.png`)} onClick={() => props.onCellClick(props.row, props.column)} alt="red" />
                     </div>
-                );
+                )
+            } else {
+                if (props.side) {
+                    return (
+                        <div className="black-cell"  >
+                            <img src={require(`./pieces/${props.cell.side}/${props.cell.name}.png`)} onClick={() => props.onCellClick(props.row, props.column)} alt="red" />
+                        </div>
+                    );
+                } else {
+                    return (
+                        <div className="black-cell" >
+                            <img src={require(`./pieces/${props.cell.side}/${props.cell.name}.png`)} onClick={() => props.onCellClick(props.row, props.column)} alt="red" />
+                        </div>
+                    )
+                }
+            }
+        } else if (props.cell.side === 'white') {
+            if (props.cell.clicked) {
+                return (
+                    <div className="white-cell-clicked"  >
+                        <img src={require(`./pieces/${props.cell.side}/${props.cell.name}.png`)} onClick={() => props.onCellClick(props.row, props.column)} alt="red" />
+
+                    </div>
+                )
             } else {
                 return (
-                    <div className="black-cell" >
+                    <div className="white-cell" >
+
+                        <img src={require(`./pieces/${props.cell.side}/${props.cell.name}.png`)} onClick={() => props.onCellClick(props.row, props.column)} alt="red" />
+
+                    </div>
+                )
+            }
+        } else {
+            return (
+
+                <div className="cell" onClick={() => props.onCellClick(props.row, props.column)}>{props.cell.name}</div>
+            );
+        }
+    } else {
+        if (props.cell.side === 'black') {
+            if (props.cell.clicked) {
+                return (
+                    <div className="light-black-cell-clicked"  >
+                        <img src={require(`./pieces/${props.cell.side}/${props.cell.name}.png`)} onClick={() => props.onCellClick(props.row, props.column)} alt="red" />
+                    </div>
+                )
+            } else {
+                if (props.side) {
+                    return (
+                        <div className="light-black-cell"  >
+                            <img src={require(`./pieces/${props.cell.side}/${props.cell.name}.png`)} onClick={() => props.onCellClick(props.row, props.column)} alt="red" />
+                        </div>
+                    );
+                } else {
+                    return (
+                        <div className="light-black-cell" >
+                            <img src={require(`./pieces/${props.cell.side}/${props.cell.name}.png`)} onClick={() => props.onCellClick(props.row, props.column)} alt="red" />
+                        </div>
+                    )
+                }
+            }
+        } else if (props.cell.side === 'white') {
+            if (props.cell.clicked) {
+                return (
+                    <div className="light-white-cell-clicked"  >
+                        <img src={require(`./pieces/${props.cell.side}/${props.cell.name}.png`)} onClick={() => props.onCellClick(props.row, props.column)} alt="red" />
+
+                    </div>
+                )
+            } else {
+                return (
+                    <div className="light-white-cell" >
                         <img src={require(`./pieces/${props.cell.side}/${props.cell.name}.png`)} onClick={() => props.onCellClick(props.row, props.column)} alt="red" />
                     </div>
                 )
             }
-        }
-    } else if (props.cell.side === 'white') {
-        if (props.cell.clicked) {
-            return (
-                <div className="white-cell-clicked"  >
-                    <img src={require(`./pieces/${props.cell.side}/${props.cell.name}.png`)} onClick={() => props.onCellClick(props.row, props.column)} alt="red" />
-
-                </div>
-            )
         } else {
             return (
-                <div className="white-cell" >
-
-                    <img src={require(`./pieces/${props.cell.side}/${props.cell.name}.png`)} onClick={() => props.onCellClick(props.row, props.column)} alt="red" />
-
-                </div>
-            )
+                <div className="light-cell" onClick={() => props.onCellClick(props.row, props.column)}>{props.cell.name}</div>
+            );
         }
-    } else {
-        return (
-
-            <div className="cell" onClick={() => props.onCellClick(props.row, props.column)}>{props.cell.name}</div>
-        );
     }
 }
 
